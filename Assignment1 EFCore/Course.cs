3b51133b742c7a6d7363e37d829756bbb9eb6b28
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-25 22:28:40 +0200

    itiDBContext & STudent & Courses

---
+++ Assignment1 EFCore/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_EFCore
+{
+    class Course
+    {
+
+        public int id { get; set; }
+
+        public TimeOnly Duration { get; set; }
+
+        public required string name { get; set; }
+
+        public string? Description { get; set; }
+        public int Top_id { get; set; }
+
+    }
+}
