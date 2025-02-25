537dd753952fe960bae89ac753565178b9be826b
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-25 21:55:26 +0200

    created DBContext

---
+++ Assignment1 EFCore/itiDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_EFCore
+{
+    class itiDBContext : DbContext
+    {
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=IIZEEX;Database=iti;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
